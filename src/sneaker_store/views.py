8f1535f971c1b9ced4ece1e617adbcc28993432d
Aleksandr <alex19950318@gmail.com>
2025-10-25 19:25:03 +0300

    Create new app (Sneaker_store) and templates

---
+++ src/sneaker_store/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def template_view(request):
+    return render(
+        request=request,
+        template_name="base.html",
+    )
+
+
+def home_view(request):
+    return render(
+        request=request,
+        template_name="home.html"
+    )
+
+
+def about_us(request):
+    return render(
+        request=request,
+        template_name="about.html"
+    )
+
+
+def contact_info(request):
+    return render(
+        request=request,
+        template_name="contact_info.html"
+    )
+
+
+def comment_user(request):
+    return render(
+        request=request,
+        template_name="comment_user.html"
+    )
