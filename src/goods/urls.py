865e2b7616267584cd63122c5e4006329ae1cdc1
Aleksandr <alex19950318@gmail.com>
2025-11-05 20:15:17 +0300

    Pagination has been done on the page.

--- src/goods/urls.py
+++ src/goods/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 
 from .views import (
     catalog,
+    product,
     product_detail,
 )
 
 
 urlpatterns = [
-    path('', catalog, name='index'),
+    path('', catalog, {'category_slug': 'all'}, name='catalog_all'),
+    path('<slug:category_slug>/', catalog, name='index'),
+    path('<slug:category_slug>/<int:page>/', catalog, name='index'),
     path('product/<slug:slug>/', product_detail, name='product_detail'),
 ]
