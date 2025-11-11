97c4b0ec3bfa1aa57304c273b91afcd496981bdb
Aleksandr <alex19950318@gmail.com>
2025-11-11 23:58:10 +0300

    Update paginator and create filter product

--- src/goods/urls.py
+++ src/goods/urls.py
@@ -11,6 +11,6 @@ from .views import (
 urlpatterns = [
     path('', catalog, {'category_slug': 'all'}, name='catalog_all'),
     path('<slug:category_slug>/', catalog, name='index'),
-    path('<slug:category_slug>/<int:page>/', catalog, name='index'),
     path('product/<slug:slug>/', product_detail, name='product_detail'),
+    path('product/<slug:product_slug>/', product, name="product"),
 ]
